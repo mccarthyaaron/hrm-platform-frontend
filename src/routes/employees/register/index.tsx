3b53a64ef8ee85510cb46f2658b59a7a7b1674d9
mccarthyaaron <mccarthymutebi@gmail.com>
2025-10-29 12:54:26 +0300

    render the employee registration form at the route employees/register

--- src/routes/employees/register/index.tsx
+++ src/routes/employees/register/index.tsx
@@ -1,9 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router';
 
+import { EmployeeRegistrationForm } from '../../../components/employees/employee-registration-form.component';
+
 export const Route = createFileRoute('/employees/register/')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>register employee</div>;
+  return <EmployeeRegistrationForm />;
 }
